7ed45265abb6b44e0ca97e35b1d2b01352608d76
Fergal Mulligan <fshaq4@gmail.com>
2024-10-23 13:58:37 +0000

    Fix set up errors

--- diyblog/urls.py
+++ diyblog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for diy_blog project.
+URL configuration for diyblog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from hello_world import views as index_views
+from django.urls import path
+from blog.views import my_blog
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('blog/', my_blog, name='blog'),
 ]
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diy_blog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diyblog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
