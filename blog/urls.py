327eef517f8efcb9406fda07a1ac79aeb90e9632
Fergal Mulligan <fshaq4@gmail.com>
2024-11-10 10:01:59 +0000

    Solve about page issues and working perfectly now

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 #Add URL pattern
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
