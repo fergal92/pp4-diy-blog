f4ed7e81f21d5ea4639eebce4f02e07ede723652
Fergal Mulligan <fshaq4@gmail.com>
2024-11-16 17:12:58 +0000

    Fix error with env variables cloudinary local development

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -166,9 +168,3 @@ CSRF_TRUSTED_ORIGINS = [
 cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME')
 api_key = os.getenv('CLOUDINARY_API_KEY')
 api_secret = os.getenv('CLOUDINARY_API_SECRET')
-
-cloudinary.config(
-    cloud_name=cloud_name,
-    api_key=api_key,
-    api_secret=api_secret
-)
